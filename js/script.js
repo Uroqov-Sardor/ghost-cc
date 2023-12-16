bc22f1915ab37f589e0591517516aa618604674a
sardordev20 <uroqovsardor510@gmail.com>
2023-12-16 18:22:09 +0500

    project finishing...

---
+++ js/script.js
@@ -0,0 +1,11 @@
+document.addEventListener("DOMContentLoaded", () => {
+  "use strict";
+  const loader = document.querySelector(".loader");
+
+  setTimeout(() => {
+    loader.style.opacity = 0;
+    setTimeout(() => {
+      loader.style.display = "none";
+    }, 1000);
+  }, 2000);
+});
